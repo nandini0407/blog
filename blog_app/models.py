c78a1c180569a85d39b9743fa1cfdb19c1372390
nandini0407 <nandini0407@gmail.com>
2017-05-02 16:41:33 -0700

    add post model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey('auth.User')
+    created_at = models.DateTimeField(default=timezone.now)
+    published_at = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
