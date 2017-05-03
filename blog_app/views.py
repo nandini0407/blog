a6f6e51cc21dc377d690d33d5cf3ac62dce229ee
nandini0407 <nandini0407@gmail.com>
2017-05-02 19:31:34 -0700

    add dynamic template

--- blog_app/views.py
+++ blog_app/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 
+from .models import Post
+
 def post_list(request):
-    return render(request, 'blog_app/post_list.html', {})
+    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('published_at')
+    return render(request, 'blog_app/post_list.html', { 'posts' : posts })
