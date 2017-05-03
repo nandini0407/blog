b1b269329ab570cf671fc90e72eeb9969f507483
nandini0407 <nandini0407@gmail.com>
2017-05-03 00:28:54 -0700

    add edit and update features

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<p_id>[0-9]+)/$', views.post_detail, name='post_detail')
+    url(r'^(?P<p_id>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^new/$', views.post_new, name='post_new'),
+    url(r'^create/$', views.post_create, name='post_create'),
+    url(r'^(?P<p_id>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^(?P<p_id>[0-9]+)/update$', views.post_update, name='post_update'),
+    # url(r'^(?P<p_id>[0-9]+)/destroy$', views.post_destroy, name='post_destroy')
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import PostForm
 
 # Create your views here.
 
@@ -15,3 +16,30 @@ def post_list(request):
 def post_detail(request, p_id):
     post = get_object_or_404(Post, pk=p_id)
     return render(request, 'blog_app/post_detail.html', { 'post' : post })
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog_app/post_new.html', { 'form' : form })
+
+def post_create(request):
+    form = PostForm(request.POST)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_at = timezone.now()
+        post.save()
+        return redirect('post_detail', p_id=post.pk)
+
+def post_edit(request, p_id):
+    post = get_object_or_404(Post, pk=p_id)
+    form = PostForm(instance=post)
+    return render(request, 'blog_app/post_edit.html', { 'form' : form, 'post' : post })
+
+def post_update(request, p_id):
+    old_post = get_object_or_404(Post, pk=p_id)
+    form = PostForm(request.POST, instance=old_post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        # prveious data from the record will still be the same, hence we can directly save it
+        post.save()
+        return redirect('post_detail', p_id=post.pk)
